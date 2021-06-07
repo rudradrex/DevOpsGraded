f1cf89bf555048f26c1ba2ee4a8f989bce599101
rudradrex <singhrudramani@gmail.com>
2021-06-07 10:41:18 +0530

    first commit

---
+++ src/main/java/com/hcl/assignment/JenkinsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAssignmentApplication.class, args);
+	}
+
+}
